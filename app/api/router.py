3623e25e48c961032b9b15d08c27ffc628ccb42c
Oskar Jonsson <oj222jq@student.lnu.se>
2025-05-20 14:28:22 +0200

    tested file upload with swagger ui

--- app/api/router.py
+++ app/api/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.api.router import router as resources_router
 # import any other routers…
+from . import upload
 
 api_router = APIRouter()
-api_router.include_router(resources_router, prefix="/resources", tags=["resources"])
+# api_router.include_router(resources_router, prefix="/resources", tags=["resources"])
 # e.g. api_router.include_router(users_router, prefix="/users", tags=["users"])
-
-
+api_router.include_router(upload.router, tags=["upload"])
